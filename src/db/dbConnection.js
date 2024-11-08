d71cf65110d8b3a001ea6f6ef296eb08c87d44c8
parthVachhani06 <parthvachhani2212@gmail.com>
2024-11-09 00:11:28 +0530

    add bulid

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
-const config = require('../config/config');  // Ensure the config contains the correct MONGODB_URL
+const config = require('../config/config'); 
+const dotenv = require("dotenv");
+ // Ensure the config contains the correct MONGODB_URL
 
 const connectDB = async () => {
   try {
     console.log("Connecting to MongoDB with URL: " + config.mongodb.url); // For debugging
-    await mongoose.connect(config.mongodb.url, config.mongodb.options);
+    await mongoose.connect(process.env.MONGODB_URL, config.mongodb.options);
     console.log('MongoDB connected successfully');
   } catch (error) {
     console.error('Database connection error: ', error);
